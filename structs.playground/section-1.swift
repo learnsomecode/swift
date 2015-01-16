e3f14985b105f6565775744636e974cab8be0e04
Josh <joshception@icloud.com>
2015-01-15 22:39:29 -0800

    oh yea added more shit dudesSs

--- structs.playground/section-1.swift
+++ structs.playground/section-1.swift
@@ -2,7 +2,17 @@
 
 // create struct around contact
 struct Contact {
+    // obviously if these attributes are constants you can't reassign them
     var firstName: String
+    
+    // customly initialize this shit if you want idk
+    init (firstName: String) {
+        self.firstName = firstName
+    }
+    
+    func returnName() -> String {
+        return self.firstName
+    }
 }
 
 // declare variable with my info
@@ -16,3 +26,6 @@ person.firstName = "bobby"
 
 // and now it's reassigned :)
 person.firstName
+
+// call the returnName function (same as .firstName in this example)
+person.returnName()
