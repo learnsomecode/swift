52bc169addea6aa23b6bb7f424439b47be897567
Josh <joshception@icloud.com>
2015-01-17 15:27:52 -0800

    added an  Xcode project

---
+++ FunFacts/FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Josh Trommel on 2015-01-17.
+//  Copyright (c) 2015 Josh Trommel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
